3d0587e12640449dae53b2c980e3216c8470b8c1
Kris Huang <zou_406143883@qq.com>
2023-04-19 15:04:25 +0800

    微调
    更新架构图

--- fe/src/components/SearchInput.vue
+++ fe/src/components/SearchInput.vue
@@ -28,7 +28,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineEmits } from "vue";
+import { ref } from "vue";
 import { SearchOutlined } from "@ant-design/icons-vue";
 
 defineProps({
